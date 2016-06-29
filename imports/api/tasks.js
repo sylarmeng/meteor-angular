2d443631801527fe06b760248e23bbec2c7dfaff
sylarmeng <sylar_2000@163.com>
2016-06-29 08:32:54 +0800

    meteor angular controller usage
    method-1 reactive attach scope in controller,add helper to this
    method-2 add helper to $scope

---
+++ imports/api/tasks.js
@@ -0,0 +1,4 @@
+import { Mongo } from 'meteor/mongo';
+ 
+// export const Article = new Mongo.Collection('article');
+export const Tasks = new Mongo.Collection('tasks');
---
+++ server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+// import '../imports/api/dbsource.js';
+import '../imports/api/tasks.js';
+// import { Article } from '../imports/api/dbsource.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
